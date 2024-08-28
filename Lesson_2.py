26a6fc0f0d4ca721ca3ebf6e30edcd0c6488f8be
Alex <alivsaz@gmail.com>
2024-08-28 13:03:44 +0300

    Add files via upload

---
+++ Lesson_2.py
@@ -0,0 +1,7 @@
+# Переменные
+number_homework = 12
+number_hours = 1.5
+course_title = 'Python'
+time_task = number_hours / number_homework
+print('Курс:',course_title,', всего задач:',number_homework,', затрачено часов: ',
+      number_hours,', среднее время выполнения',time_task,'часа.')
