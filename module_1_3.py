67a55a6f9288d4197bbdb3b2296854f84f301901
Alex <alivsaz@gmail.com>
2024-08-29 11:03:16 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Динамическая типизация
+name = 'Александр'
+print('Name:'+name)
+age = 53
+print('Age:'+str(age))
+age += 12
+print('New age:'+str(age))
+is_student = True
+print('Is_student:'+str(is_student))
