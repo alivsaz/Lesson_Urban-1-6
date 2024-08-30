1ff89b78f99786c03eb5928dcaf8a5a17eb34a03
Alex <alivsaz@gmail.com>
2024-08-30 11:56:15 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+# Организация программ и методы строк
+my_string = input('Введите любую строку с пробелами: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
