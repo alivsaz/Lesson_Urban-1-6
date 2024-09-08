c4a40388b5549ea94bf2f55b3690f7efcd355fc5
Alex <alivsaz@gmail.com>
2024-09-08 21:07:51 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,12 @@
+# Слишком древний шифр
+def pair_of_numbers(pair):
+    result = pair
+
+code = int(input('Введите код (число от 3 до 20): '))
+
+
+
+
+
+result = pair_of_numbers(code)
+print(f'\nРезультат - комбинация "КОД - ПАРОЛЬ": \n{code} - {result} ')
