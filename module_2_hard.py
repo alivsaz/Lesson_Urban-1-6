a7f4105c287f547f72ccddb902dbb3904169b3f9
Alex <alivsaz@gmail.com>
2024-09-09 12:38:44 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,12 @@
 # Слишком древний шифр
 def pair_of_numbers(pair):
-    result = pair
+    password = ''
+    for i in range(1,19):
+        for j in range(1,pair):
+            if pair%(i+j) == 0 and i<j:
+                password = password + str(i) + str(j)
+    return (password)
 
 code = int(input('Введите код (число от 3 до 20): '))
-
-
-
-
-
 result = pair_of_numbers(code)
 print(f'\nРезультат - комбинация "КОД - ПАРОЛЬ": \n{code} - {result} ')
