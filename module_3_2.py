0576c781e91a32157186df97ce145df00506808d
Alex <alivsaz@gmail.com>
2024-09-11 14:37:37 +0300

    Update module_3_2.py - Добавил проверку истинности электронной почты

--- module_3_2.py
+++ module_3_2.py
@@ -1,23 +1,27 @@
 # Рассылка писем
+
 def send_email(message, recipient, *, sender = "university.help@gmail.com"):
-    email_strings = ('.com', '.ru', '.net', '@')
-    for i in range(len(email_strings)):
-        if email_strings[i] in recipient or email_strings[i] in sender:
-            if recipient == sender:
-                print(f'\nНельзя отправить письмо самому себе!')
-                break
-            elif sender == "university.help@gmail.com":
-                print(f'\nПисьмо успешно отправлено с адреса {sender} '
-                      f'на адрес {recipient}.')
-                break
-            else:
-                print(f'\nНЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено '
-                      f'с адреса {sender} на адрес {recipient}.')
-                break
+    email_strings = ['.com', '.ru', '.net']
+    if find_string_email(email_strings, recipient) == True:
+        if recipient == sender:
+            print(f'\nНельзя отправить письмо самому себе!')
+        elif sender == "university.help@gmail.com":
+            print(f'\nПисьмо успешно отправлено с адреса {sender} '
+                  f'на адрес {recipient}.')
         else:
-            print(f'\nНевозможно отправить письмо с адреса {sender} '
-                  f'на адрес {recipient}')
+            print(f'\nНЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено '
+                  f'с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'\nНевозможно отправить письмо с адреса {sender} '
+              f'на адрес {recipient}')
+
+def find_string_email (string, email):
+    find_email = False
+    for i in range(len(string)):
+        if (email.find(string[i]) != -1) and (email.find('@') != -1):
+            find_email = True
             break
+    return(find_email)
 
 send_email('Это сообщение для проверки связи',
            'vasyok1337@gmail.com')
